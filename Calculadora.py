a2fa6f50e8f73233e8152acf8801dbc271c28fa6
Hadson00 <hadson798@gmail.com>
2024-03-18 22:04:15 -0300

    Change code

--- Calculadora.py
+++ Calculadora.py
@@ -3,60 +3,53 @@ import math
 class Calculator:
     def __init__(self):
         pass
-    def sum(self, number1, number2):
+    def sum1(self, number1:int, number2:int)->int:
         return number1 + number2
-    def subtraction(self, number1, number2):
+    def subtraction(self, number1:int, number2:int)->int:
         return number1 - number2
-    def multiplication(self, number1, number2):
+    def multiplication(self, number1:int, number2:int)->int:
         return number1 * number2
-    def division(self, number1, number2):
-        if number2 == 0:
+    def division(self, number1:int, number2:int)->float:
+        if number1 == 0 or number2 == 0:
             return "Error"
         return number1 / number2
-    def percentage(self, value, percentage):
+    def percentage(self, value:int, percentage:float)->float:
         return (value * percentage) / 100
-    def squareroot(self, number1):
+    def squareroot(self, number1:int):
         return math.sqrt(number1)
-    def potentiation(self, base, exponent):
+    def potentiation(self, base:int, exponent:int)->int:
         return base ** exponent
 number1 = int(input('Digite um número: '))
-selector = str(input('Selecione o operador: '))
+selector = str(input('Selecione o operador (+, -, *, /, %, raiz, ^): '))
 
-x = Calculator()
+calc = Calculator()
  
 if selector == '+':
     number2 = int(input('Digite o segundo número: '))
-    result = x.sum(number1, number2)
-    print(result)
+    print(calc.sum1(number1, number2))
 
 elif selector == '-':
-    number2 = int(input('Digite o segundo número: '))
-    result = x.subtraction(number1, number2)
-    print(result)
+    number2 = int(input('Digite o segundo número: ')) 
+    print(calc.subtraction(number1, number2))
 
 elif selector == '*':
-    number2 = int(input('Digite o segundo número: '))
-    result = x.multiplication(number1, number2)
-    print(result)
+    number2 = int(input('Digite o segundo número: ')) 
+    print(calc.multiplication(number1, number2))
 
 elif selector == '/':
-    number2 = int(input('Digite o segundo número: '))
-    result = x.division(number1, number2)
-    print(result)
+    number2 = int(input('Digite o segundo número: ')) 
+    print(calc.division(number1, number2))
 
 elif selector == '%':
-    number2 = int(input('Digite o segundo número: '))
-    result = x.percentage(number1, number2)
-    print(result)
+    number2 = int(input('Digite o segundo número: ')) 
+    print(calc.percentage(number1, number2))
 
-elif selector == 'raiz':
-    result = x.squareroot(number1)
-    print(result)
+elif selector == 'raiz': 
+    print(calc.squareroot(number1))
 
 elif selector == '^':
-    number2 = int(input('Digite o segundo número: '))
-    result = x.potentiation(number1, number2)
-    print(result)
+    number2 = int(input('Digite o segundo número: ')) 
+    print(calc.potentiation(number1, number2))
 
 else:
     print("Operação inválida!")
